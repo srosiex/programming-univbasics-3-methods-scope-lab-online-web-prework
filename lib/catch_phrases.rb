dc9ee0b386254ac9d03545a9f36711ccc13db1d2
Sarah H <sarahrose.henderson@gmail.com>
2019-08-23 16:14:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,9 +3,15 @@ puts "It's-a me, Mario!"
 end
 
 def toadstool
+  status = "phrase"
   puts 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
 def link
+  status = "phrase"
   puts "It's Dangerous To Go Alone! Take This."
 end
+
+def starfox
+  status = "phrase"
+  puts "Do A Barrel Roll!"
