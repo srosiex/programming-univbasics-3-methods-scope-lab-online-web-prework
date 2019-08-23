0015e9daa562f4cafd54804d8b7c323dcafe7f19
Sarah H <sarahrose.henderson@gmail.com>
2019-08-23 16:32:15 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,20 +1,17 @@
-
-
 def mario
-puts "It's-a me, Mario!"
+  phrase = "It's-a me, Mario!"
+  puts phrase
 end
 
 def toadstool
-  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts "#{phrase}"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
 end
 
 def link
-  phrase = "It's Dangerous To Go Alone! Take This."
-  puts "#{phrase}"
+  puts "It's Dangerous To Go Alone! Take This."
 end
 
-def starfox
-phrase = "Do A Barrel Roll!"
-puts "#{phrase}"
+def any_phrase(phrase)
+  puts phrase
 end
